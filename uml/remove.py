56d1ca0be478e222f3848f1477bcfd5f5a96d8c1
toantran292 <tranthaitoan998@gmail.com>
2024-03-15 01:22:42 +0700

    Add python script to remove backgound

---
+++ uml/remove.py
@@ -0,0 +1,34 @@
+import os
+from bs4 import BeautifulSoup
+
+def get_svg_files(folder_path):
+    contents = os.listdir(folder_path)
+    files = [file for file in contents if os.path.isfile(os.path.join(folder_path, file)) and file.endswith(".svg")]
+    return files
+
+def delete_text(svg_content):
+    soup = BeautifulSoup(svg_content, 'xml') 
+    elements = soup.find_all(text="UNREGISTERED")  
+    for element in elements:
+        parent = element.parent
+        if parent.name == 'text':
+            parent.decompose() 
+    return str(soup)
+
+def edit_svg_file(file_path): 
+    with open(file_path, 'r') as file:
+        svg_content = file.read()
+    modified_svg_content = delete_text(svg_content)
+    with open(file_path, 'w') as file:
+        file.write(modified_svg_content)
+
+# file = 'UseCaseTongQuat.svg'
+
+current_path = os.path.dirname(os.path.realpath(__file__))
+files = get_svg_files(current_path)
+
+for file in files:
+    file_path = os.path.join(current_path, file)
+    edit_svg_file(file_path)
+    print(f"{file} edited successfully")
+
