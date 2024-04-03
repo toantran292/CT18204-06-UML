d8682ecd4c0226ca7724a18f1cf23b0bc633dd66
toantran292 <tranthaitoan998@gmail.com>
2024-04-03 12:42:49 +0700

    .

---
+++ data.js
@@ -0,0 +1,30 @@
+const thongtin = {
+  "idDoi": 1,
+  "idGiaiDau": 1,
+  "idHangMuc": 1,
+  "data": [
+    {
+      "tenTran": "TranMot",
+      :
+      "cacLoiViPham": [
+        {
+          "thoigian": "12:00",
+          "noidung": "sdjaldjksadlk",
+          "idVDV": 1,
+        },
+        //...
+      ],
+      //cacLoiViPham: [1,2,3,4,5...]
+      "thanhTich": [
+        {
+          "thoiGian": "12:00",
+          "diemSo": 1.5,
+          "idVDV": 1,
+        },
+        //...
+      ]
+      //"thanhTich": [1,2,3,4,...]
+    },
+    //...
+  ]
+}
